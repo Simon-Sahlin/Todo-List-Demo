db8925ec7f1727ae0875d500b549137cf063d762
Simon-Sahlin <simonsahlin@somin.se>
2024-04-22 16:36:59 +0200

    Improve default lists

--- src/dataHandler.js
+++ src/dataHandler.js
@@ -10,7 +10,6 @@ localStorage.setItem("colorSetting", "#a4509b");
 let dataHandler = (function(){
 
     function loadData(){
-
         console.log("Loading Data...");
         let loadedData = getItem("data", 0);
         if (loadedData == 0){
@@ -26,13 +25,35 @@ let dataHandler = (function(){
             });
         });
 
-        // console.log(parsedData);
         console.log("Done Loading Data!");
-
     }
+
 
     function loadDefaultData(){
         console.log("Loading Default Data...")
+
+        let defaultList = new taskList("Example List - Chores");
+        defaultList.addTask("Do the dishes");
+        defaultList.addTask("Vacuum", "Its very important that you vacuum the basement, it hasn't been done in a while. You don't need to vacuum the living room.");
+        defaultList.addTask("Buy groceries", "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed officiis, inventore ab omnis voluptates architecto possimus dicta, excepturi perspiciatis quas odit optio dolorum illo minima delectus laborum fuga eveniet autem natus nostrum quia dolores accusamus? Pariatur laborum aperiam quis, dolores iure autem nesciunt!", 0, false, true);
+        defaultList.addTask("Write shopping list", "", 0, true, false);
+        
+
+        let anotherList = new taskList("Example List - Work")
+        anotherList.addTask("Super Important report!!", "", 0, false, true)
+        anotherList.addTask("Send Emails", "", new Date(), true, true)
+        anotherList.addTask("To be done today!", "", new Date(), false, false)
+        let today = new Date();
+        anotherList.addTask("To be done this week!", "", new Date(today.getFullYear(), today.getMonth(), today.getDate()+7), false, false)
+        anotherList.addTask("Had to be done yesterday!!", "", new Date(today.getFullYear(), today.getMonth(), today.getDate()-1), false, false)
+        anotherList.addTask("Buy Supplies", "", new Date(today.getFullYear(), today.getMonth()-1, today.getDate()-2), true, true)
+
+        console.log("Done Loading Default Data!");
+    }
+
+
+    function loadDebugData(){
+        console.log("Loading Debug Data...")
 
         let defaultList = new taskList("Default");
         defaultList.addTask("Yeet");
@@ -46,9 +67,9 @@ let dataHandler = (function(){
         let today = new Date();
         anotherList.addTask("be done this week!", "", new Date(today.getFullYear(), today.getMonth(), today.getDate()+7), false, false)
 
-        console.log("Done Loading Default Data!");
+        console.log("Done Loading Debug Data!");
     }
-    
+
 
     function saveData(){
         console.log("Saving Data...");
